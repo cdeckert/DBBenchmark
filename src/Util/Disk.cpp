e964faf6c15aecee67ae684c29b901fa74d1a731
Meatad.GER <meatwad.ger@gmail.com>
2014-11-18 11:04:09 +0100

    results

--- src/Util/Disk.cpp
+++ src/Util/Disk.cpp
@@ -50,8 +50,8 @@ Disk::Disk(std::string path)
 void Disk::open(std::string path)
 {
 	this->path = path;
-	this->fd = open64(this->path.data(), O_RDWR |  O_LARGEFILE | O_DIRECT); //O_SYNC); // O_DIRECT
-	perror("OPEN");
+	this->fd = open64(this->path.data(), O_RDWR |  O_LARGEFILE | O_SYNC); // O_DIRECT
+	if(fd == -1) perror("Disk.cpp: OPEN");
 }
 
 
